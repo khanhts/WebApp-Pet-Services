6983eaa3c1ca6bdda03010c2ff6682a5fa1d3b90
Nguyen Quang Trai <nguyenquangtrai.100203@gmail.com>
2025-04-13 22:25:26 +0700

    datlich

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,12 +1,15 @@
+
 const mongoose = require('mongoose');
 
 const appointmentSchema = new mongoose.Schema({
+    appointmentDate: { type: Date, required: true },
+    appointmentTime: { type: String, required: true },
     pet: { type: String, required: true },
     ownerName: { type: String, required: true },
     phone: { type: String, required: true },
     email: { type: String, required: true },
-    appointmentDate: { type: Date, required: true, index: true }, // Đặt index để tăng hiệu suất truy vấn
-    reason: { type: String }
-}, { timestamps: true });
+    service: { type: String, required: true }, // Dịch vụ
+    reason: { type: String, required: false }
+});
 
 module.exports = mongoose.model('Appointment', appointmentSchema);
